aade7253c81d4c0b7b681651c3b79a11d7556b5e
Mike McCreavy <mike@mccreavy.com>
2013-05-23 00:48:45 -0700

    save

--- js/input.js
+++ js/input.js
@@ -2,7 +2,43 @@
   var log = window.dirg.debug ? window.dirg.debug.log : console.log;
 
   // date: 2013-05-19, author: mccreavy
-  function create() {
+  function create(p) {
+    return {
+      'inputElement': 'needOne',
+      'key': []
+    };
+  }
+
+  // date: 2013-05-23, author: mccreavy
+  function addListeners() {
+    var self = this;
+    $(this.inputElement).keydown(function(e) {
+      self.key[e.keyCode] = 1;
+    });
+    $(this.inputElement).keyup(function(e) {
+      self.key[e.keyCode] = 0;
+    });
+    /*
+    $(this.element).mousemove(function(e) {
+      self.cursor.x = e.offsetX;
+      self.cursor.y = e.offsetY;
+      for (var i = 0 ; i < self.mouseMoveHook.length ; i++) {
+        self.mouseMoveHook[i](e);
+      }
+    });
+    $(this.element).click(function(e) {
+      if (self.eventQueue) {
+        self.eventQueue.push(
+          xmm.Event(
+            'click',
+            xmm.Math.Point(e.clientX, e.clientY)
+          )
+        );
+      }
+      e.preventDefault();
+      return false;
+    });
+    */
   }
 
   window.dirg.input = {

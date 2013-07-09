2de3de31c01eb580931ec7f7c466df848779ad65
Mike McCreavy <mike@mccreavy.com>
2013-07-09 00:18:31 -0700

    Ketchup

---
+++ js/ChatWidget.js
@@ -0,0 +1,22 @@
+(function() {
+
+  // date: 2013-07-08, author: mccreavy
+  function ChatWidget(p) {
+    var o = {
+      'container': p.container,
+      'render': render,
+      'add': add
+    };
+    return o;
+  };
+
+  // date: 2013-07-08, author: mccreavy
+  function render() {
+  }
+
+  // date: 2013-07-08, author: mccreavy
+  function add() {
+  }
+
+  window.dirg.ChatWidget = ChatWidget;
+})();

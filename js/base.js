0699cedde34a6b7c2ba11b0e275d31fb1c073c86
Mike McCreavy <mike@mccreavy.com>
2013-07-11 16:51:01 -0700

    Allow default account to be set by URL param

--- js/base.js
+++ js/base.js
@@ -1,3 +1,11 @@
 window.dirg = {
-  assert: function(x, msg) { if (!x) { throw msg; } }
+  assert: function(x, msg) { if (!x) { throw msg; } },
+
+  getUrlParameter: function(name) {
+      return decodeURIComponent(
+          (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)')
+           .exec(location.search)||[,""])[1].replace(/\+/g, '%20')
+          )||null;
+  }
+
 };
